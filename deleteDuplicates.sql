d93dc9fabdbf73223b4813be081b96984e611ee3
malex30 <59666512+malex30@users.noreply.github.com>
2023-02-04 22:57:28 -0600

    196 Delete duplicate emails

---
+++ deleteDuplicates.sql
@@ -0,0 +1,27 @@
+-- 196.
+-- Table: Person
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | email       | varchar |
+-- +-------------+---------+
+-- id is the primary key column for this table.
+-- Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+-- Write an SQL query to delete all the duplicate emails, keeping only one unique email with the smallest id. Note that you are supposed to write a DELETE statement and not a SELECT one.
+
+-- After running your script, the answer shown is the Person table. The driver will first compile and run your piece of code and then show the Person table. The final order of the Person table does not matter.
+
+-- The query result format is in the following example.
+
+-- This solution is slower than most, only beating just under 70% of others.
+
+DELETE FROM Person
+WHERE id NOT IN
+(SELECT MIN(id)
+FROM Person
+GROUP BY email HAVING COUNT(email)>=1
+)
